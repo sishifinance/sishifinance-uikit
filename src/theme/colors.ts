719ddd74a995a89f1ce240d77e5ce27c7dd83c94
Sishi Dev <sishifinancedev@gmail.com>
2021-03-23 19:07:01 +0700

    update

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,9 +2,9 @@ import { Colors } from "./types";
 
 export const baseColors = {
   failure: "#ED4B9E",
-  primary: "#aa8929",
-  primaryBright: "#aa8929",
-  primaryDark: "#aa8929",
+  primary: "#37a635",
+  primaryBright: "#37a635",
+  primaryDark: "#37a635",
   secondary: "#7645D9",
   success: "#31D0AA",
   warning: "#FFB237",
